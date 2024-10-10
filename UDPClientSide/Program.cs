66d1a369c027fb1e511059df6c7aed60a26816c1
Shaki_bf42 <Shaki_bf42@STHQ0118-16>
2024-10-10 11:39:29 +0400

    Add project files.

---
+++ UDPClientSide/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Client Side");
+
+var client = new UdpClient();
+var endPoint = new IPEndPoint(IPAddress.Loopback, 27001);
+
+while (true)
+{
+    //var msg = Console.ReadLine();
+    //var bytes = Encoding.UTF8.GetBytes(msg);
+    //client.Send(bytes, bytes.Length, endPoint);
+    Console.WriteLine("File Path-ni daxil edin:");
+    string FilePath=Console.ReadLine();
+
+    using(var readFs =new FileStream(FilePath, FileMode.Open, FileAccess.Read))
+    {
+        int len = 0;
+        var buffer = new byte[1024];
+        while((len = readFs.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            client.Send(buffer,len,endPoint);
+        }
+    }
+    Console.WriteLine("photo sended");
+    client.Close();
+
+}
---
+++ UDPServerSide/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("server");
+
+var server = new UdpClient(27001);
+var remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+while (true)
+{
+    var bytes = server.Receive(ref remoteEndPoint);
+   
+    //Console.WriteLine($"{remoteEndPoint}: {str}");
+    int count = 0;
+    var str = Encoding.UTF8.GetString(bytes);
+    //string destinationPath = $@"C:\Users\Shaki_bf42\source\repos\UDPClientSide\UDPServerSide\Photos\image_{++count}.jpg";
+    //var directory = Directory.CreateDirectory($@"C:\Users\Shaki_bf42\source\repos\UDPClientSide\UDPServerSide\{remoteEndPoint.Address.ToString}\image_{++count}.jpg");
+  //  var destination = $@"C:\Users\Shaki_bf42\source\repos\UDPClientSide\UDPServerSide\{remoteEndPoint.Address}\image_{++count}.jpg";
+
+    var destination = $@"C:\Users\Shaki_bf42\source\repos\UDPClientSide\UDPServerSide\{remoteEndPoint.Address}\image_{++count}.jpg";
+
+   
+    var directoryPath = Path.GetDirectoryName(destination);
+    Directory.CreateDirectory(directoryPath); 
+
+    //destination = $@"{destination}\{remoteEndPoint.Address}\image_{++count}.jpg";
+
+
+    using (var fs=new FileStream(destination, FileMode.Append, FileAccess.Write))
+    {
+        
+        fs.Write(bytes, 0, bytes.Length);
+    }
+    
+}
